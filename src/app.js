4fac162fe2388fdd1f4423f5f6f59f2b43f62a93
Gabriella Santos <mgabriellassantos@gmail.com>
2022-11-19 19:18:09 -0300

    new project

---
+++ src/app.js
@@ -0,0 +1,16 @@
+import express from "express";
+import db from "./config/dbConnect.js"
+import routes from "./routes/index.js"
+
+db.on("error", console.log.bind(console, 'Erro de conexão'))
+db.once("open", () => {
+  console.log('conexão com o banco feita com sucesso')
+})
+
+const app = express();
+app.use(express.json())
+routes(app);
+
+export default app
+
+//conecta com o banco e passa instâncias do express para utilizar as rotas
